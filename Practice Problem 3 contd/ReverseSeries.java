50e95932e86c8578ff3ce2102cdb68e4351c4696
Arnob Chowdhury <arnob11chowdhury@gmail.com>
2025-05-22 21:32:07 +0600

    Added the code for reversing a given series using array and loop

---
+++ Practice Problem 3 contd/ReverseSeries.java
@@ -0,0 +1,13 @@
+public class ReverseSeries {
+    public static void main(String[] args) {
+
+        int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println("Reversed series:");
+
+        for (int i = 9; i >= 0; i--) {
+
+            System.out.print(arr[i] + " ");
+        }
+    }
+}

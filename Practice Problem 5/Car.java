99528ff5f810bbc73cb366704a40ce13eb487247
Arnob Chowdhury <arnob11chowdhury@gmail.com>
2025-05-23 12:52:21 +0600

    Added the code for designing a Car class based on the given description

---
+++ Practice Problem 5/Car.java
@@ -0,0 +1,56 @@
+public class Car {
+
+    String owner;
+    String brand;
+    String serialNumber;
+    boolean isRunning;
+    int fuelLevel;
+
+    public Car (String owner, String brand, String serialNumber, int fuelLevel) {
+
+        this.owner = owner;
+        this.brand = brand;
+        this.serialNumber = serialNumber;
+        this.fuelLevel = fuelLevel;
+        this.isRunning = false;
+    }
+
+    public void startCar() {
+
+        if (fuelLevel > 0) {
+
+            isRunning = true;
+            System.out.println("Car started");
+        }
+        else {
+
+            System.out.println("Fuel is empty. Cyannot start car");
+        }
+    }
+
+    public void stopCar() {
+
+        isRunning = false;
+        System.out.println("Car stopped");
+    }
+
+    public void checkFuel() {
+
+        System.out.println("Fuel level: " + fuelLevel + " litres");
+    }
+
+    public static void main(String[] args) {
+        
+        Car myCar = new Car("Arnob", "Toyota", "ABCD1234", 10);
+
+        System.out.println(myCar.owner);
+        System.out.println(myCar.brand);
+        System.out.println(myCar.serialNumber);
+        myCar.startCar();
+        System.out.println("Car status: " + (myCar.isRunning ? "Running" : "Stopped"));
+        myCar.stopCar();
+        System.out.println("Car status: " + (myCar.isRunning ? "Running" : "Stopped"));
+        myCar.checkFuel();
+    }
+}
+
